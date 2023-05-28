a920b304f97f9e172a3aaf9b72d174fda8fe2f59
Ihor Sukhorada <ihorsukhorada@outlook.com>
2023-05-28 09:54:59 +0200

    Add Menu screen

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,13 +1,15 @@
 import GameScreen from "../screens/GameScreen";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import MenuScreen from "../screens/MenuScreen";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function RootNavigator(): JSX.Element {
     return (
         <NavigationContainer>
-            <Navigator>
+            <Navigator initialRouteName="Menu">
+                <Screen name="Menu" component={MenuScreen} />
                 <Screen name="Game" component={GameScreen} />
             </Navigator>
         </NavigationContainer>
