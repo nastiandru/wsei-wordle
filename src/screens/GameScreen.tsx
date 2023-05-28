63c0c190918480c5752b106446e680a968135b40
nastiandru <nastia.andr070703@gmail.com>
2023-05-29 00:34:53 +0300

    theme to game screen

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,10 +1,13 @@
 import {StyleSheet, View} from "react-native";
 import Keyboard from "../components/Keyboard";
 import Board from "../components/Board";
+import React, {useContext} from "react";
+import { ThemeContext } from "../ThemeContext";
 
 export default function GameScreen() : JSX.Element {
+    const { isDarkMode } = useContext(ThemeContext);
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, isDarkMode && styles.darkContainer]}>
             <Board/>
             <Keyboard/>
         </View>
@@ -13,7 +16,11 @@ export default function GameScreen() : JSX.Element {
 
 const styles: any = StyleSheet.create({
     container: {
-        justifyContent: "space-between",
-        flex: 1,
-    }
+      justifyContent: "space-between",
+      flex: 1,
+      backgroundColor: "#ffffff", // Світла тема
+    },
+    darkContainer: {
+      backgroundColor: "#000000", // Темна тема
+    },
 })
