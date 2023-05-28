9b757fcbe337c05aa781f9cc0609ecdd19021161
nastiandru <nastia.andr070703@gmail.com>
2023-05-28 20:56:10 +0300

    add splas hscreen

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,17 +1,23 @@
-import GameScreen from "../screens/GameScreen";
-import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import MenuScreen from "../screens/MenuScreen";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SplashScreen from '../screens/SplashScreen';
+import MenuScreen from '../screens/MenuScreen';
+import GameScreen from '../screens/GameScreen';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function RootNavigator(): JSX.Element {
-    return (
-        <NavigationContainer>
-            <Navigator initialRouteName="Menu">
-                <Screen name="Menu" component={MenuScreen} />
-                <Screen name="Game" component={GameScreen} />
-            </Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Navigator initialRouteName="Splash">
+        <Screen name="Splash" component={SplashScreen} />
+        <Screen name="Menu" component={MenuScreen} />
+        <Screen name="Game" component={GameScreen} />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
+
+
