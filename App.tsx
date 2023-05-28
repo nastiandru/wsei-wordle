7acf6d2877e673e236837b88c8e50e9d0372ea7b
nastiandru <nastia.andr070703@gmail.com>
2023-05-29 00:36:47 +0300

    add global theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { store } from "./src/store";
 import { Provider } from "react-redux";
 import RootNavigator from "./src/navigators/RootNavigator";
+import { ThemeProvider } from "./src/ThemeContext";
+
 
 export default function App(): JSX.Element {
   return (
-      <Provider store={store}>
-          <RootNavigator/>
-      </Provider>
+    <Provider store={store}>
+    <ThemeProvider>
+      <RootNavigator />
+    </ThemeProvider>
+  </Provider>
+   
   )
 }
