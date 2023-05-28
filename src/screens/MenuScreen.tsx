351f323fc3f0cbc047a6567180866161d6a4b483
nastiandru <nastia.andr070703@gmail.com>
2023-05-29 01:45:51 +0300

    change color

--- src/screens/MenuScreen.tsx
+++ src/screens/MenuScreen.tsx
@@ -1,36 +1,49 @@
 import { BackHandler, Button, View, StyleSheet } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import { ThemeContext } from "../ThemeContext";
 
 export default function MenuScreen({ navigation }: { navigation: any }) : JSX.Element {
     const { isDarkMode } = useContext(ThemeContext);
+    const [newGameButtonColor, setNewGameButtonColor] = useState("#778899");
+
+    const handleNewGamePress = () => {
+        const previousColor = newGameButtonColor; 
+        setNewGameButtonColor("#00FF00"); 
+
+        setTimeout(() => {
+            setNewGameButtonColor(previousColor); 
+        }, 1000); 
+
+        navigation.navigate('Game');
+    };
+
     return (
         <View style={[styles.container, isDarkMode && styles.darkContainer]}>
       <View style={styles.buttonContainer}>
-        <Button
-          title="New Game"
-          color="#f194ff"
-          onPress={() => navigation.navigate('Game')}
-        />
+      <Button
+                    title="New Game"
+                    color={newGameButtonColor} 
+                    onPress={handleNewGamePress} 
+                />
       </View>
       <View style={styles.buttonContainer}>
         <Button
           title="Settings"
-          color="#f194ff"
+          color="#CD853F"
           onPress={() => navigation.navigate('Settings')}
         />
       </View>
       <View style={styles.buttonContainer}>
         <Button
           title="About"
-          color="#f194ff"
+          color="#CD853F"
           onPress={() => navigation.navigate('About')}
         />
       </View>
       <View style={styles.buttonContainer}>
         <Button
           title="Exit"
-          color="#f194ff"
+          color="#CD853F"
           onPress={() => BackHandler.exitApp()}
         />
       </View>
